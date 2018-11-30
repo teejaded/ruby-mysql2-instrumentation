bc57860a0eafb761c3cd8b72c30719376d94ccb8
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-11-30 10:50:58 -0500

    Initial commit

---
+++ lib/mysql2/instrumentation.rb
@@ -0,0 +1,7 @@
+require "mysql2/instrumentation/version"
+
+module Mysql2
+  module Instrumentation
+    # Your code goes here...
+  end
+end
---
+++ lib/mysql2/instrumentation/version.rb
@@ -0,0 +1,5 @@
+module Mysql2
+  module Instrumentation
+    VERSION = "0.1.0"
+  end
+end
